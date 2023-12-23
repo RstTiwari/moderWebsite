6fe40ccfa93bfe31a0da8f9605a271a8c4cfc5a3
RstTiwari <iamrrt2121@gmail.com>
2023-12-23 18:12:30 +0530

    added crosuel

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import Navbar from "./Component/Navbar"
+import React from "react";
+import HomeImage from "./Assets/Homebanner.jpg";
+import { Carousel } from "antd";
+import { useMediaQuery } from "@mui/material";
 
 const Home = () => {
-  return (
-    <div style={{
-        display:"flex",
-        flexDirection:"row"
-    }}>
-    </div>
-  )
-}
+    return (
+        <div
+            style={{
+                marginTop: "auto",
+                height: "100vh",
+                textAlign: "center",
+                alignItems: "center",
+                justifyContentL: "center",
+            }}
+        >
+            <Carousel autoplay>
+                <div className="sliderImageDiv">
+                    <h1 >Welocme to the Welding House</h1>
+                    <img alt="slider1" src={HomeImage} />
+                </div>
+                <div className="sliderImageDiv">
+                    <h1>Welocme to the Welding House</h1>
+                    <img alt="slider1" src={HomeImage} />
+                </div>
+            </Carousel>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
