afa4b91862006f8ac88be06249e28749cb1c7a3d
RstTiwari <iamrrt2121@gmail.com>
2024-03-30 12:24:50 +0530

    Added Transition Effect

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,32 +1,46 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { Button } from 'antd';
-import Typography from '@mui/material/Typography';
-import { CardHeader, IconButton } from '@mui/material';
-import {useNavigate}  from "react-router-dom"
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { Button } from "antd";
+import Typography from "@mui/material/Typography";
+import { CardHeader, IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-export default function MediaCard({image, title,details,actionName,actionLink}) {
+export default function MediaCard({
+    image,
+    title,
+    details,
+    actionName,
+    actionLink,
+}) {
     const navigate = useNavigate();
     const handleButtonClick = () => {
         navigate(actionLink);
     };
-  return (
-      <Card sx={{ maxWidth: 350 }}>
-          <CardHeader sx={{ color: "#000" }} title={title} />
-          <CardMedia sx={{ height: 140 }} image={image} title="green iguana" />
-          <CardContent>
-              <Typography variant="body2" color="#333">
-                  {details}
-              </Typography>
-          </CardContent>
-              <IconButton>
-                  <Button size ={"large"}  style={{backgroundColor:"#006635" ,color:"#fff"}} onClick={handleButtonClick}>
-                      {actionName}
-                  </Button>
-              </IconButton>
-      </Card>
-  );
+    return (
+        <Card sx={{ maxWidth: "100%" }}>
+            <CardHeader sx={{ color: "#000" }} title={title} />
+            <CardMedia
+                sx={{ height: 500 }}
+                image={image}
+                title="green iguana"
+            />
+            <CardContent>
+                <Typography variant="body2" color="#333">
+                    {details}
+                </Typography>
+            </CardContent>
+            {/* <IconButton>
+                <Button
+                    size={"large"}
+                    style={{ backgroundColor: "#006635", color: "#fff" }}
+                    onClick={handleButtonClick}
+                >
+                    {actionName}
+                </Button>
+            </IconButton> */}
+        </Card>
+    );
 }
--- src/Component/Service.js
+++ src/Component/Service.js
@@ -1,27 +1,76 @@
 import React from "react";
 import Card from "./Card";
-import data from "../data/data"
+import data from "../data/data";
+import { Carousel } from "antd";
+const { serviceData } = data;
 
 const Service = () => {
     return (
         <>
-            <h1 className="headerTerxt" style={{backgroundColor:"#006635"}}>Our Service</h1>
-            <div className="service_component">
-                {data.serviceData.map((item) => {
-                    return (
-                        <div className="singleCard">
-                            <Card
-                                title={item.title}
-                                image={item.image}
-                                details={item.details}
-                                actionName={item.actionName}
-                                actionLink={item.actionLink}
-                            />
-                        </div>
-                    );
-                })}
-            </div>
-            
+            <h1 className="headerTerxt" style={{ backgroundColor: "#006635" }}>
+                Our Service
+            </h1>
+            <Carousel
+                autoplay={true}
+                autoplaySpeed={1500}
+                effect={"scrollx"}
+                dotPosition={"bottom"}
+            >
+                <div className="singleCard">
+                    <Card
+                        title={serviceData[0].title}
+                        image={serviceData[0].image}
+                        details={serviceData[0].details}
+                        actionName={serviceData[0].actionName}
+                        actionLink={serviceData[0].actionLink}
+                    />
+                </div>
+                <div className="singleCard">
+                    <Card
+                        title={serviceData[0].title}
+                        image={serviceData[0].image}
+                        details={serviceData[0].details}
+                        actionName={serviceData[0].actionName}
+                        actionLink={serviceData[0].actionLink}
+                    />
+                </div>
+                <div className="singleCard">
+                    <Card
+                        title={serviceData[1].title}
+                        image={serviceData[1].image}
+                        details={serviceData[1].details}
+                        actionName={serviceData[1].actionName}
+                        actionLink={serviceData[1].actionLink}
+                    />
+                </div>
+                <div className="singleCard">
+                    <Card
+                        title={serviceData[2].title}
+                        image={serviceData[2].image}
+                        details={serviceData[2].details}
+                        actionName={serviceData[2].actionName}
+                        actionLink={serviceData[2].actionLink}
+                    />
+                </div>
+                <div className="singleCard">
+                    <Card
+                        title={serviceData[3].title}
+                        image={serviceData[3].image}
+                        details={serviceData[3].details}
+                        actionName={serviceData[3].actionName}
+                        actionLink={serviceData[3].actionLink}
+                    />
+                </div>
+                <div className="singleCard">
+                    <Card
+                        title={serviceData[4].title}
+                        image={serviceData[4].image}
+                        details={serviceData[4].details}
+                        actionName={serviceData[4].actionName}
+                        actionLink={serviceData[4].actionLink}
+                    />
+                </div>
+            </Carousel>
         </>
     );
 };
